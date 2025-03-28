7452dd7d6228570d9d58c6798786fd6f4458300f
giovanni.zol <giovanni.zol.24@stud.itsaltoadriatico.it>
2025-03-28 12:00:04 +0100

    implementato express e chiamate api

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ const server = express();
 const port = 3000;
 
 server.get('/', (req, res) => {
-    res.send('Hello Worldgdgfdgfdgf');
+  res.send('Hello world');
 });
 
+server.get('/json', (req, res) => {
+  const rispostaJson = {"nome": "pippo"};
+
+  res.send(rispostaJson);
+})
+
 server.listen(port, () => {
-    console.log('server in ascolto');
+  console.log('server in ascolto!')
 })
