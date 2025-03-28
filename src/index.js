fe5263e37c1ef2b673afca418c09fd4eaa0d23c4
giovanni.zol <giovanni.zol.24@stud.itsaltoadriatico.it>
2025-03-28 15:37:21 +0100

    creati metodi -2- per chiamate json

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ const express = require('express');
 const server = express();
 const port = 3000;
 
+// visualizzare json metodo 1
+server.use('/data/home.json', express.static('data'));
+
+// visualizzare json metodo 2
+server.get('/tecnologie', (req, res) => {
+    let data=require("../data/home.json")
+    res.status(200).json(data);
+  })
+
 server.get('/', (req, res) => {
   res.send('Hello world');
 });
